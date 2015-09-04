cf0c75a1784bee64f99277a87735b5e17b07ecef
DeborahK <deborahk@insteptech.com>
2015-09-04 10:15:55 -0700

    Check in of Angular session information.

---
+++ MovieHunter Take5 - Forms/MovieHunter/app/movies/movieEditCtrl.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("movieHunter")
+        .controller("MovieEditCtrl",
+                    ["movieResource",
+                        "$routeParams",
+                     MovieEditCtrl]);
+
+    function MovieEditCtrl(movieResource, $routeParams) {
+        var vm = this;
+
+        vm.movie = "";
+        vm.title = "";
+        vm.message = "";
+
+        movieResource.get({ movieId: $routeParams.movieId },
+            function (data) {
+                vm.movie = data;
+
+                if (vm.movie.movieId) {
+                    vm.title = vm.movie.title;
+                }
+                else {
+                    vm.title = "New Movie";
+                }
+            });
+
+        vm.submit = function (isValid) {
+            if (isValid) {
+                vm.movie.$save(function (data) {
+                    vm.message = "Save successful.";
+                });
+            } else {
+                vm.message = "Please correct the validation errors and try saving again.";
+            }
+        };
+    }
+}());
